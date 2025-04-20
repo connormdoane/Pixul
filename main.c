e82ea5cf3d57c3cf4339f7508ba3518b63b03ddc
Connor <doaneconnorm@gmail.com>
2025-04-20 09:53:54 -0600

    update example to work with newest version

--- main.c
+++ main.c
@@ -4,23 +4,21 @@
 int main()
 {
   // Create the initial window
-  P_Window window;
-
-  P_Create("Smiley", &window, 800, 800, 10, 10);
+  P_Create("Smiley", 800, 800, 10, 10);
 
   // Fill the image with white
-  P_Clear(&window, (P_Color){0, 0, 0, 255});
+  P_Clear((P_Color){0, 0, 0, 255});
 
   // Set a few specific pixels for a smiley face :)
-  P_Set(&window, 2, 2, (P_Color){255, 255, 255, 255});
+  P_Set(2, 2, (P_Color){255, 255, 255, 255});
 
-  P_Set(&window, 4, 2, (P_Color){255, 255, 255, 255});
+  P_Set(4, 2, (P_Color){255, 255, 255, 255});
 
-  P_Set(&window, 1, 4, (P_Color){255, 255, 255, 255});
-  P_Set(&window, 2, 5, (P_Color){255, 255, 255, 255});
-  P_Set(&window, 3, 5, (P_Color){255, 255, 255, 255});
-  P_Set(&window, 4, 5, (P_Color){255, 255, 255, 255});
-  P_Set(&window, 5, 4, (P_Color){255, 255, 255, 255});
+  P_Set(1, 4, (P_Color){255, 255, 255, 255});
+  P_Set(2, 5, (P_Color){255, 255, 255, 255});
+  P_Set(3, 5, (P_Color){255, 255, 255, 255});
+  P_Set(4, 5, (P_Color){255, 255, 255, 255});
+  P_Set(5, 4, (P_Color){255, 255, 255, 255});
 
   // Draw the updated image to the screen (must be called each time a change is made)
   P_Update(&window);
